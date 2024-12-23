768e4eeed69f55cd87b21fcdbebf045799f1ad14
flynno <luke.laz.flynn@gmail.com>
2024-12-22 20:17:06 -0700

    updated userRoute formats

--- dist/routes/api/userRoutes.js
+++ dist/routes/api/userRoutes.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { getUsers, createUser, getSingleUser, updateUser, removeUser, addFriend, removeFriend } from "../../controllers/userControllers.js";
+import { getUsers, createUser, getSingleUser, updateUser, removeUser, addFriend, removeFriend, } from "../../controllers/userControllers.js";
 const router = Router();
 // api/users
-router.route('/')
+router.route("/")
     .get(getUsers) // Get all users
     .post(createUser); // Create a new user
 // api/users/:userId
-router.route('/:userId')
+router.route("/:userId")
     .get(getSingleUser) // Get a single user
     .put(updateUser) // Update a single user
     .delete(removeUser); // Remove a single user
 // api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+router.route("/:userId/friends/:friendId")
+    .post(addFriend)
+    .delete(removeFriend);
 export { router as userRouter };
