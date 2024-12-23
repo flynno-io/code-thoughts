140b0c94e0cb18251884aede3d149e383e708efe
flynno <luke.laz.flynn@gmail.com>
2024-12-22 21:03:42 -0700

    added checks for missiong/incorrect reactionId

--- src/controllers/thoughtControllers.ts
+++ src/controllers/thoughtControllers.ts
@@ -109,16 +109,29 @@ export const addReaction = async (req: Request, res: Response) => {
 // Remove a reaction from a thought -> DELETE /thoughts/:thoughtId/reactions
 export const removeReaction = async (req: Request, res: Response) => {
 	try {
-		const thought = await Thought.findOneAndUpdate(
-			{ _id: req.params.thoughtId },
-			{ $pull: { reactions: { reactionId: req.body.reactionId } } },
-			{ runValidators: true, new: true }
+		const thought = await Thought.findOne(
+			{ _id: req.params.thoughtId }
 		)
 		if (!thought) {
 			res.status(404).json({ message: "No thought with this Id" })
-		} else {
-			res.json(thought)
-		}
+    } else {
+      // Check if the reaction exists
+      const isReactionFound = thought.reactions.find(
+        (reaction) => reaction.reactionId.toString() === req.body.reactionId, 
+        { runValidators: true, new: true }
+      )
+      console.log(isReactionFound)
+      if (!isReactionFound) {
+        res.status(404).json({ message: "No reaction with this Id" })
+      } else {
+        thought.reactions = thought.reactions.filter(
+          (reaction) => reaction.reactionId.toString() !== req.body.reactionId
+        )
+        console.log(thought.reactions)
+        await thought.save()
+        res.json(thought)
+      }
+    }
 	} catch (error: any) {
 		res.status(400).json({ message: error.message })
 	}
